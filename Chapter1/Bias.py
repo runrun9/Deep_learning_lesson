1f56c5334accc81213c19f74fe30f170e98e509c
Lun <lunsan951@gmail.com>
2017-06-06 01:27:35 +0900

    correction notation

--- Chapter1/Bias.py
+++ Chapter1/Bias.py
@@ -1,14 +1,17 @@
 import numpy as np
-def AND(x1,x2):
-    x = np.array([x1,x2])     #入力
-    w = np.array([0.5,0.5]) #重み
-    b = -0.7               #バイアス
+
+
+def AND(x1, x2):
+    x = np.array([x1, x2])      # 入力
+    w = np.array([0.5, 0.5])    # 重み
+    b = -0.7                    # バイアス
     if np.sum(x*w) + b > 0:
         return 1
     else:
         return 0
 
-print(AND(0,0))
-print(AND(0,1))
-print(AND(1,0))
-print(AND(1,1))
+
+print(AND(0, 0))
+print(AND(0, 1))
+print(AND(1, 0))
+print(AND(1, 1))
--- Chapter3/softmax.py
+++ Chapter3/softmax.py
@@ -1,6 +1,6 @@
 # ソフトマックス関数による出力層の設計
-
 import numpy as np
+
 
 def softmax(a):
     c = np.max(a)
@@ -9,7 +9,8 @@ def softmax(a):
     y = exp_a / sum_exp_a
     return y                    # 各入力の指数 / 全入力の指数
 
-a = np.array([0.3,2.9,4.0])     # 出力層の入力値
+
+a = np.array([0.3, 2.9, 4.0])   # 出力層の入力値
 y = softmax(a)
 print(y)
 print(np.sum(y))                # 正規化により合計は1
